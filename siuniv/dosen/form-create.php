0ef83d07f4c4c041c66d28bbce9b536061e7cc99
JosephineTanA <98298035+JosephineTanA@users.noreply.github.com>
2022-09-15 13:42:32 +0700

    Add files via upload

---
+++ siuniv/dosen/form-create.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <h1>Tambah Data Dosen</h1>
+    <form action="create.php" method="POST">
+        Nama dosen : <input type="text" name="nama_dosen"><br>
+        No. Telp : <input type="text" name="telp"><br>
+        <input type="button" value="simpan"name="btnSimpan">
+    </form>
+</body>
+</html>
